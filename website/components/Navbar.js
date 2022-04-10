b5110a9ba3c2abe044841843afd4dbd15e271839
KitoTeam <info@kito.online>
2022-04-10 19:18:10 -0400

    still working

--- website/components/Navbar.js
+++ website/components/Navbar.js
@@ -18,11 +18,11 @@ const Navbar = () => {
                 <div className="text-white text-2xl lg:text-4xl name-font ml-2 font-semibold">Heather M. Murphy</div>
             </div>
       
-            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+            <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer hide-menu'>
                 <Image src={menuIcon} height={30} width={30} name={open ? 'close':'menu'} />
             </div>
 
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute pt-8 md:pt-0 md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-2 transition-all duration-500 ease-in ${open ? 'top-10 ':'top-[-490px]'}`}>
+            <ul className={`pb-12 absolute pt-8 bg-black z-[-1] left-0 w-full desktop-nav pl-2 transition-all duration-500 ease-in ${open ? 'top-10 ':'top-[-490px]'}`}>
     
                 <a href="/" className="md:ml-5 text-lg ">
                     <li className="text-yellow-400  hover:text-yellow-200 font-semibold mt-2 md:my-0 duration-500 ">Home</li>
--- website/components/ServiceOne.js
+++ website/components/ServiceOne.js
@@ -6,23 +6,29 @@ const ServiceOne = () => {
     return (
         <div className='h-[100vh] px-2 relative pt-[3rem] w-full'>
             
-            <div className="flex items-center justify-center">
+            <div className="flex items-center lg:mb-5 justify-center">
                 <div className="border-2 border-yellow-300 h-[.25px] w-14 md:w-24 mr-5"></div>
-                <h1 className="text-center text-4xl md:text-5xl title-font font-bold ">Elevated Living</h1>
+                <h1 className="text-center text-4xl sm:text-5xl title-font font-bold ">Elevated Living</h1>
                 <div className="border-2 border-yellow-300 h-[.25px] w-14 md:w-24 ml-5"></div>
             </div>
 
-            <div className="space-y-6 mx-auto h-full w-full md:flex md:items-center md:justify-around ">
+            <div className="space-y-6 mx-auto h-full w-full md:space-y-0 md:flex md:items-center md:justify-around lg:px-[2.5rem] lg:pt-[10rem] xl:px-[4rem] ">
 
                 {/* Left Container */}
-                <div className="px-2 w-full md:-mt-[27rem] ">
+                <div className="px-2 w-full md:-mt-[26rem] lg:-mt-[16rem] xl:-mt-[30rem] ">
 
-                    <div className="md:-mt-5">
+                    <div className="py-2 md:py-0 md:-mt-5 lg:mb-[1rem] ">
                         <h2 className="text-2xl sm:text-3xl md:text-4xl font-[Poppins] italic font-semibold text-yellow-300">Follow Me and</h2>
                         <p className="text-xl sm:text-2xl md:text-3xl -mt-1 md:-mt-0 ml-4 font-[Poppins] italic text-gray-900">Find Yourself</p>
                     </div>
 
-                    <p className="font-[Poppins] text-md pt-3 max-w-[800px] text-gray-500 ml-4 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, hic facilis officia cum animi cumque laboriosam accusantium libero nostrum expedita sequi porro temporibus consequatur aliquid, voluptatum quam eligendi, fugit quo.</p>
+                    <p className="font-[Poppins] text-md pt-3 max-w-[600px] text-gray-500 md:text-lg ml-4 md:ml-2 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, hic facilis officia cum animi cumque laboriosam accusantium libero nostrum expedita sequi porro temporibus consequatur aliquid, voluptatum quam eligendi, fugit quo.</p>
+                    <p className="font-[Poppins] text-md pt-3 max-w-[600px] text-gray-500 md:text-lg ml-4 md:ml-2 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, hic facilis officia cum animi cumque laboriosam accusantium libero nostrum expedita sequi porro temporibus consequatur aliquid, voluptatum quam eligendi, fugit quo.</p>
+                    <p className="font-[Poppins] text-md pt-3 max-w-[600px] text-gray-500 md:text-lg ml-4 md:ml-2 ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, hic facilis officia cum animi cumque laboriosam accusantium libero nostrum expedita sequi porro temporibus consequatur aliquid, voluptatum quam eligendi, fugit quo.</p>
+
+                    <div className="flex items-center justify-center mt-5 max-w-[600px] ">
+                        <button className="border-2 w-[max-content] px-2 py-1 border-yellow-400 font-[Poppins] text-center text-md md:text-xl lg:px-4 lg:py-2 ">Learn More</button>
+                    </div>
 
                     
 
@@ -32,7 +38,7 @@ const ServiceOne = () => {
                 {/* Right Container */}
                 <div className="p-2 ">
 
-                    <div className="h-full mx-auto w-[300px] md:w-[400px] md:-mt-[10rem]">
+                    <div className="h-full mx-auto w-[300px] md:w-[400px] md:-mt-[26rem] lg:-mt-[16rem] xl:-mt-[22rem]  ">
                         <img src='/vert1.jpg' className='w-full' alt='personal' />
                     </div>
                 </div>
